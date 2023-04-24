8201d2ebb411e01ef0e6ee3da1fe961adcf13910
Subham5401 <mishrasubham21081998@gmail.com>
2023-04-25 01:02:45 +0530

    issue #6: Create MCQA for basic array search methods: Array.indexOf(), Array.lastIndexOf(), Array.includes()

---
+++ code/024/script.js
@@ -0,0 +1,5 @@
+const fruits = ["apple", "banana", "orange", "grape", "apple", "kiwi"];
+const index = fruits.indexOf("orange");
+const lastIndex = fruits.lastIndexOf("apple");
+const result = fruits.includes("grape");
+console.log('index: ', index, 'lastIndex: ', lastIndex, 'result: ', result);
