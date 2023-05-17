2b8d316239541f581fb2d08cd5ac2f712f04a2db
JP-GECT <100013491+JP-GECT@users.noreply.github.com>
2023-05-17 20:09:43 +0530

    made the requested changes

---
+++ code/035/script.js
@@ -0,0 +1,15 @@
+function User(username) {
+    this.username = username;
+  
+    this.updateUsername = (newName) => {
+      this.username = newName;
+    };
+  }
+  
+  const user1 = new User('kirandash');
+  const user2 = new User('bgwebagency');
+  
+  user1.updateUsername('-website');
+  user2.updateUsername('-app');
+  
+  console.log(user1.username + user2.username);
