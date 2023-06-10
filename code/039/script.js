21063350df7de2f3653021c51a0692c2c4db909f
RahulRana701 <rahulrana2001a@gmail.com>
2023-06-10 13:08:12 +0530

    question-39-added

---
+++ code/039/script.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+  };
+  
+  const { name, age, city } = person;
+  
+  console.log(name); 
+  console.log(age); 
+  console.log(city); 
